b6a072e14bfa8aeacb4f4df21340feccf990d951
SuperScript <2528622340@qq.com>
2026-03-20 10:11:43 +0800

    chore: common.item: 更安全的 C/S 隔离

--- api/common/item.py
+++ api/common/item.py
@@ -1,12 +1,14 @@
 # coding=utf-8
 from ...internal import InClientEnv
-from ..client import GetItemTags as ClientGetItemTags
-from ..server import GetItemTags as ServerGetItemTags
 
 
 def GetItemTags(item_id, aux_value):
     # type: (str, int) -> set[str]
     if InClientEnv():
+        from ..client import GetItemTags as ClientGetItemTags
+
         return ClientGetItemTags(item_id, aux_value)
     else:
+        from ..server import GetItemTags as ServerGetItemTags
+
         return ServerGetItemTags(item_id, aux_value)
