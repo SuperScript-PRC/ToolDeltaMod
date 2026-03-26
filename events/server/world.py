322ada990e6f095fa6c341f9aeea98436f94b751
SuperScript <2528622340@qq.com>
2026-03-26 10:15:22 +0800

    feat: events.server.world: OnSimTickServerEvent

--- events/server/world.py
+++ events/server/world.py
@@ -4,11 +4,11 @@ from ..basic import ServerEvent
 class DelServerPlayerEvent(ServerEvent):
     name = "DelServerPlayerEvent"
 
-    id = '' # type: str
+    id = ""  # type: str
     """玩家id"""
-    isTransfer = False # type: bool
+    isTransfer = False  # type: bool
     """是否是切服时退出服务器，仅用于Apollo。如果是True，则表示切服时退出服务器；若是False，则表示退出网络游戏"""
-    uid = 0 # type: int
+    uid = 0  # type: int
     """玩家的netease uid，玩家的唯一标识"""
 
     @classmethod
@@ -24,15 +24,15 @@ class DelServerPlayerEvent(ServerEvent):
 class ChunkAcquireDiscardedServerEvent(ServerEvent):
     name = "ChunkAcquireDiscardedServerEvent"
 
-    dimension = 0 # type: int
+    dimension = 0  # type: int
     """ 区块所在维度 """
-    chunkPosX = 0 # type: int
+    chunkPosX = 0  # type: int
     """ 区块的x坐标，对应方块X坐标区间为[x * 16, x * 16 + 15] """
-    chunkPosZ = 0 # type: int
+    chunkPosZ = 0  # type: int
     """ 区块的z坐标，对应方块Z坐标区间为[z * 16, z * 16 + 15] """
-    entities = [] # type: list[str]
+    entities = []  # type: list[str]
     """ 随区块卸载而从世界移除的实体id的列表。注意事件触发时已经无法获取到这些实体的信息，仅供脚本资源回收用。 """
-    blockEntities = [] # type: list[dict]
+    blockEntities = []  # type: list[dict]
     """ 随区块卸载而从世界移除的自定义方块实体的坐标的列表，列表元素dict包含posX，posY，posZ三个int表示自定义方块实体的坐标，blockName表示方块的identifier，包含命名空间及名称。注意事件触发时已经无法获取到这些方块实体的信息，仅供脚本资源回收用。 """
 
     @classmethod
@@ -60,13 +60,13 @@ class ChunkAcquireDiscardedServerEvent(ServerEvent):
 class ChunkLoadedServerEvent(ServerEvent):
     name = "ChunkLoadedServerEvent"
 
-    dimension = 0 # type: int
+    dimension = 0  # type: int
     """ 区块所在维度 """
-    chunkPosX = 0 # type: int
+    chunkPosX = 0  # type: int
     """ 区块的x坐标，对应方块X坐标区间为[x * 16, x * 16 + 15] """
-    chunkPosZ = 0 # type: int
+    chunkPosZ = 0  # type: int
     """ 区块的z坐标，对应方块Z坐标区间为[z * 16, z * 16 + 15] """
-    blockEntities = [] # type: list[dict]
+    blockEntities = []  # type: list[dict]
     """ 随区块加载而加载进世界的自定义方块实体的坐标的列表，列表元素dict包含posX，posY，posZ三个int表示自定义方块实体的坐标，blockName表示方块的identifier，包含命名空间及名称 """
 
     @classmethod
@@ -87,3 +87,17 @@ class ChunkLoadedServerEvent(ServerEvent):
             "chunkPosZ": self.chunkPosZ,
             "blockEntities": self.blockEntities,
         }
+
+
+class OnSimTickServerEvent(ServerEvent):
+    name = "OnSimTickServerEvent"
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def unmarshal(cls, data):
+        return cls()
+
+    def marshal(self):
+        return {}
