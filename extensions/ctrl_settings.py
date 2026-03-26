64f8b3eff8ff5c08c989aa3dc2af8445a1de86c1
SuperScript <2528622340@qq.com>
2026-03-26 22:39:45 +0800

    feat: 更好的 Control::自定义按键识别方法

--- extensions/ctrl_settings.py
+++ extensions/ctrl_settings.py
@@ -8,14 +8,29 @@ from ...tooldelta.api.client import (
     GetKeyboardEnum,
     GetGamepadKeyEnum,
 )
+from ...tooldelta.events.client import (
+    OnCustomGamepadChangedEvent,
+    OnCustomKeyChangedEvent,
+    OnCustomGamepadPressInGame,
+    OnCustomKeyPressInGame,
+)
+from ...tooldelta.general import ClientInitCallback
+
+loaded_controls = {}  # type: dict[str, list[Control]]
+
+# TODO: 因为 OnCustomGamepadChangedEvent 和 OnCustomKeyChangedEvent
+# 都没有提供 category 参数, 所以请避免 name 重复
 
 
 class Control:
     def __init__(self, category, name, default_key, default_gamepad):
         # type: (str, str, int, int) -> None
+        loaded_controls.setdefault(name, []).append(self)
         _reg(category, name, default_key, default_gamepad)
         self.category = category
         self.name = name
+        self.default_key = default_key
+        self.default_gamepad = default_gamepad
         self._key_id = None
         self._gamepad_id = None
         self._ctrl_mode = None
@@ -60,6 +75,10 @@ class Control:
         else:
             return "未知按键"
 
+    def is_this(self, event):
+        # type: (OnCustomGamepadPressInGame | OnCustomKeyPressInGame) -> bool
+        return event.category == self.category and event.name == self.name
+
     @staticmethod
     def KEY():
         return GetKeyboardEnum()
@@ -67,3 +86,32 @@ class Control:
     @staticmethod
     def GAMEPAD():
         return GetGamepadKeyEnum()
+
+
+@ClientInitCallback(0)
+def onClientInit():
+    for name, ctrls in loaded_controls.items():
+        for ctrl in ctrls:
+            _reg(ctrl.category, name, ctrl.default_key, ctrl.default_gamepad)
+
+
+@OnCustomGamepadChangedEvent.Listen()
+def onGamepadChanged(event):
+    # type: (OnCustomGamepadChangedEvent) -> None
+    old_key = int(event.oldKey)
+    new_key = int(event.newKey)
+    ctrls = loaded_controls.get(event.name, [])
+    for ctrl in ctrls:
+        if ctrl._gamepad_id == old_key:
+            ctrl._gamepad_id = new_key
+
+
+@OnCustomKeyChangedEvent.Listen()
+def onKeyChanged(event):
+    # type: (OnCustomKeyChangedEvent) -> None
+    old_key = int(event.oldKey)
+    new_key = int(event.newKey)
+    ctrls = loaded_controls.get(event.name, [])
+    for ctrl in ctrls:
+        if ctrl._key_id == old_key:
+            ctrl._key_id = new_key
