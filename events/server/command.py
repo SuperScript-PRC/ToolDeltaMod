81cde882c41605f7e132d2813e46d86d849e75c5
SuperScript <2528622340@qq.com>
2026-04-03 01:48:48 +0800

    feat: CustomCommandTriggerServerEvent 新增 SetReturnParams

--- events/server/command.py
+++ events/server/command.py
@@ -5,17 +5,17 @@ from ..basic import ServerEvent
 class CustomCommandTriggerServerEvent(ServerEvent):
     name = "CustomCommandTriggerServerEvent"
 
-    command = "" # type: str
+    command = ""  # type: str
     """ 自定义命令名称，对应json中的name字段 """
-    args = [] # type: list[dict]
+    args = []  # type: list[dict]
     """ 自定义命令参数，详情见下方 """
-    variant = 0 # type: int
+    variant = 0  # type: int
     """ 表示是哪条变体，范围[0, 9]，对应json中args键中的数字，未配置变体则为0 """
-    origin = {} # type: dict
+    origin = {}  # type: dict
     """ 触发源的信息，详情见下方 """
-    return_failed = False # type: bool
+    return_failed = False  # type: bool
     """ 设置自定义命令是否执行失败，默认为False，如果执行失败，返回信息以红色字体显示 """
-    return_msg_key = "" # type: str
+    return_msg_key = ""  # type: str
     """ 设置返回给玩家或命令方块的信息，也支持通过语言文件(.lang)定义，默认值为commands.custom.success(自定义命令执行成功) """
     _orig = {}
 
@@ -47,4 +47,10 @@ class CustomCommandTriggerServerEvent(ServerEvent):
         self._orig["return_failed"] = self.return_failed = True
 
     def SetReturnMsg(self, msg):
+        # type: (str) -> None
         self._orig["return_msg_key"] = self.return_msg_key = msg
+
+    def SetReturnParams(self, *params):
+        # type: (str) -> None
+        # thanks D0FES
+        self._orig["return_params"] = list(params)
